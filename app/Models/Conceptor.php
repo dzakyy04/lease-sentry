cb8ada961269391012ba2b7c70f5cfb743aa187a
dzakyy04 <dzakylinggau@gmail.com>
2023-07-26 10:00:23 +0700

    Create a one to many relationship between conceptor and document2020

--- app/Models/Conceptor.php
+++ app/Models/Conceptor.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Document2020;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Conceptor extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function document2020s()
+    {
+        return $this->hasMany(Document2020::class);
+    }
 }
--- app/Models/Document2020.php
+++ app/Models/Document2020.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Conceptor;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Document2020 extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function conceptor()
+    {
+        return $this->belongsTo(Conceptor::class);
+    }
 }
