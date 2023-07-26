3d92a09c525746ab3b8e340097b8d61672c2ed67
dzakyy04 <dzakylinggau@gmail.com>
2023-07-26 12:51:45 +0700

    Create login view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}">
+    <!-- Page Title  -->
+    <title>Lease Sentry | {{ $title }}</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{ asset('assets/css/dashlite.css?ver=3.0.3') }}">
+    <link id="skin-default" rel="stylesheet" href="{{ asset('assets/css/theme.css?ver=3.0.3') }} ">
+</head>
+
+<body class="nk-body bg-white npc-default pg-auth">
+    <div class="nk-app-root">
+        <div class="nk-main ">
+            <div class="nk-wrap nk-wrap-nosidebar">
+                <div class="nk-content">
+                    <div class="nk-block nk-block-middle nk-auth-body  wide-xs">
+                        <div class="brand-logo text-center">
+                            <a href="#" class="logo-link">
+                                <img class="logo-light logo-img logo-img-lg"
+                                    src="{{ asset('assets/images/logo-kpknl.png') }}" alt="logo">
+                                <img class="logo-dark logo-img logo-img-lg"
+                                    src="{{ asset('assets/images/logo-kpknl.png') }}" alt="logo-dark">
+                            </a>
+                        </div>
+                        <div class="card">
+                            <div class="card-inner card-inner-lg">
+                                <div class="nk-block-head">
+                                    <div class="nk-block-head-content">
+                                        <h4 class="nk-block-title">Login</h4>
+                                        <div class="nk-block-des">
+                                            <p>Silahkan masukkan email dan password anda.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="html/index.html">
+                                    <div class="form-group">
+                                        <div class="form-label-group">
+                                            <label class="form-label" for="email">Email</label>
+                                        </div>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="email"
+                                                name="email" placeholder="Masukkan email anda">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-label-group">
+                                            <label class="form-label" for="password">Password</label>
+                                            <a class="link link-primary link-sm" href="">Lupa password?</a>
+                                        </div>
+                                        <div class="form-control-wrap">
+                                            <a href="#" class="form-icon form-icon-right passcode-switch lg"
+                                                data-target="password">
+                                                <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                                <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                            </a>
+                                            <input type="password" class="form-control form-control-lg" id="password"
+                                                name="password" placeholder="Masukkan password anda">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-control-sm custom-checkbox checked">
+                                            <input type="checkbox" class="custom-control-input" id="remember"
+                                                name="remember">
+                                            <label class="custom-control-label" for="remember">Ingat saya</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button class="btn btn-lg btn-primary btn-block">Login</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('assets/js/bundle.js?ver=3.0.3') }}"></script>
+    <script src="{{ asset('assets/js/scripts.js?ver=3.0.3') }}"></script>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::get('login', function() {
+    return view('auth.login', [
+        'title' => 'Login'
+    ]);
+});
