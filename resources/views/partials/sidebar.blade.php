a8c6a81be535be6e8a2ac79d225a7bcdd405d504
dzakyy04 <dzakylinggau@gmail.com>
2023-07-26 14:41:58 +0700

    Fixing bug active link in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -21,48 +21,53 @@
         <div class="nk-sidebar-content">
             <div class="nk-sidebar-menu" data-simplebar>
                 <ul class="nk-menu">
-                    <li class="nk-menu-item {{ Request::segment(1) == 'dashboard' ? 'active current-page' : '' }}">
+                    <li class="nk-menu-item {{ Request::segment(1) == 'dashboard' ? 'active-link' : '' }}">
                         <a href="" class="nk-menu-link">
                             <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
                             <span class="nk-menu-text">Dashboard</span>
                         </a>
                     </li>
-                    <li
-                        class="nk-menu-item has-sub {{ Request::segment(1) == 'dokumen' ? 'active current-page' : '' }}">
-                        <a href="#" class="nk-menu-link nk-menu-toggle">
-                            <span class="nk-menu-icon"><em class="icon ni ni-file-text"></em></span>
+                    <li class="nk-menu-item has-sub {{ Request::segment(1) == 'dokumen' ? 'active-link' : '' }}">
+                        <a href="#" class="nk-menu-link nk-menu-toggle" data-bs-original-title="" title="">
+                            <span class="nk-menu-icon">
+                                <em class="icon ni ni-file-text"></em>
+                            </span>
                             <span class="nk-menu-text">Dokumen</span>
                         </a>
                         <ul class="nk-menu-sub">
-                            <li class="nk-menu-item {{ Request::segment(2) == '2020' ? 'active current-page' : '' }}">
-                                <a href="" class="nk-menu-link">
-                                    <span class="nk-menu-icon"><em class="icon ni ni-bullet"></em></span>
-                                    <span class="nk-menu-text">2020</span></a>
+                            <li class="nk-menu-item">
+                                <a href="html/project-card.html" class="nk-menu-link" data-bs-original-title=""
+                                    title="">
+                                    <span class="nk-menu-text"><em class="ni ni-bullet"></em> 2020</span>
+                                </a>
                             </li>
-                            <li class="nk-menu-item {{ Request::segment(2) == '2021' ? 'active current-page' : '' }}">
-                                <a href="" class="nk-menu-link">
-                                    <span class="nk-menu-icon"><em class="icon ni ni-bullet"></em></span>
-                                    <span class="nk-menu-text">2021</span></a>
+                            <li class="nk-menu-item">
+                                <a href="html/project-card.html" class="nk-menu-link" data-bs-original-title=""
+                                    title="">
+                                    <span class="nk-menu-text"><em class="ni ni-bullet"></em> 2021</span>
+                                </a>
                             </li>
-                            <li class="nk-menu-item {{ Request::segment(2) == '2022' ? 'active current-page' : '' }}">
-                                <a href="" class="nk-menu-link">
-                                    <span class="nk-menu-icon"><em class="icon ni ni-bullet"></em></span>
-                                    <span class="nk-menu-text">2022</span></a>
+                            <li class="nk-menu-item">
+                                <a href="html/project-card.html" class="nk-menu-link" data-bs-original-title=""
+                                    title="">
+                                    <span class="nk-menu-text"><em class="ni ni-bullet"></em> 2022</span>
+                                </a>
                             </li>
-                            <li class="nk-menu-item {{ Request::segment(2) == '2023' ? 'active current-page' : '' }}">
-                                <a href="" class="nk-menu-link">
-                                    <span class="nk-menu-icon"><em class="icon ni ni-bullet"></em></span>
-                                    <span class="nk-menu-text">2023</span></a>
+                            <li class="nk-menu-item">
+                                <a href="html/project-card.html" class="nk-menu-link" data-bs-original-title=""
+                                    title="">
+                                    <span class="nk-menu-text"><em class="ni ni-bullet"></em> 2023</span>
+                                </a>
                             </li>
                         </ul>
                     </li>
-                    <li class="nk-menu-item {{ Request::segment(1) == 'konseptor' ? 'active current-page' : '' }}">
+                    <li class="nk-menu-item {{ Request::segment(1) == 'konseptor' ? 'active-link' : '' }}">
                         <a href="#" class="nk-menu-link">
                             <span class="nk-menu-icon"><em class="icon ni ni-users"></em></span>
                             <span class="nk-menu-text">Konseptor</span>
                         </a>
                     </li>
-                    <li class="nk-menu-item {{ Request::segment(1) == 'hari-libur' ? 'active current-page' : '' }}">
+                    <li class="nk-menu-item {{ Request::segment(1) == 'hari-libur' ? 'active-link' : '' }}">
                         <a href="#" class="nk-menu-link">
                             <span class="nk-menu-icon"><em class="icon ni ni-calender-date"></em></span>
                             <span class="nk-menu-text">Hari Libur</span>
