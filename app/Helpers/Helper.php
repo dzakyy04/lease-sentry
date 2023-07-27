c6ef3cf6120f65c1ff79426844732d48d141cb46
dzakyy04 <dzakylinggau@gmail.com>
2023-07-27 13:14:27 +0700

    Create gloval function dayDifference

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -18,4 +18,18 @@ class Helper
         $carbonDate = Carbon::parse($date);
         return $carbonDate->isWeekend() || self::isHoliday($date) ? false : true;
     }
+
+    public static function dayDifference($startDate, $endDate)
+    {
+        $start = Carbon::parse($startDate);
+        $end = Carbon::parse($endDate);
+
+        $difference = $start->diffInDaysFiltered(
+            function (Carbon $date) {
+                return !$date->isWeekend() && !Helper::isHoliday($date);
+            },
+            $end
+        );
+        return $difference;
+    }
 }
