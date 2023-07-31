aec7751d30675d2fd67e51fb6b3b6c4c4e1bb61d
dzakyy04 <dzakylinggau@gmail.com>
2023-07-31 15:27:10 +0700

    Add card in dashboard index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document2020;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,6 +10,10 @@ class DashboardController extends Controller
     public function index()
     {
         $title = 'Dashboard';
-        return view('dashboard.index', compact('title'));
+        $documents = Document2020::count();
+        $penjualan = Document2020::where('jenis_persetujuan', 'Penjualan')->count();
+        $penghapusan = Document2020::where('jenis_persetujuan', 'Penghapusan')->count();
+        $sewa = Document2020::where('jenis_persetujuan', 'Sewa')->count();
+        return view('dashboard.index', compact('title', 'documents', 'penjualan', 'penghapusan', 'sewa'));
     }
 }
